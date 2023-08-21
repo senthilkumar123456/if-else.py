66c989a402c2df556325293b2caf6b84b4c894f6
senthilkumar123456 <141622183+senthilkumar123456@users.noreply.github.com>
2023-08-21 12:59:10 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,50 @@
+##num1=int(input('enter the value of num1'))
+##num2=int(input('enter the value of num2'))
+##num3=int(input('enter the value of num3'))
+##if(num1>=num2)and(num1>=num3):
+##    print('num1 is largest number')
+##elif(num2>=num3):
+##    print('num2 is largest number')
+##task2
+##score=int(input('enter your score:'))
+##if(score>90)and(score<=100):
+##    print('A grade')
+##elif(score>80)and(score<=90):
+##    print('B grade')
+##elif(score>60)and(score<=80):
+##    print('C grade')
+##elif(score>50)and(score<=40):
+##    print('D grade')
+##else:
+##    print('you are fail')
+##task3
+##cp=int(input('enter the cost price of bike'))
+##if(cp>100000):
+##    print('15%')
+##elif(cp>500000):
+##    print('10%')
+##elif(cp>=500000):
+##    print('5%')
+##else:
+##    print('no road tax')
+##task4
+##num=int(input('enter the value of num'))
+##if(num==1)and(num==8):
+##    print('monday')
+##elif(num==2):
+##    print('tuesday')
+##elif(num==3):
+##    print('wednesday')
+##elif(num==4):
+##    print('thursday')
+##elif(num==5):
+##    print('friday')
+##elif(num==6):
+##    print('saturday')
+##elif(num==7):
+##    print('sunday')
+##else:
+##    print('not define 8 days')
+##task5
+
+
